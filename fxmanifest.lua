f0175758dfa596e6ab0b537fbb9c33a5d8feeeee
Mono-94 <marcecanavolt94@gmail.com>
2025-03-03 04:44:18 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,25 +6,26 @@ version '2.0.3'
 
 use_experimental_fxv2_oal 'yes'
 
+author 'Raw Paper & MONO'
+
+description 'discord: https://discord.gg/Vk7eY8xYV2 | Documents https://mono-94.github.io/mDocuments/'
+
 lua54 'yes'
 
 shared_scripts {
   '@ox_lib/init.lua',
   '@mVehicle/import.lua',
   'Config.lua',
-  'addons/**/Config.lua',
 
 }
 
 client_scripts {
   'client/**/*',
-  'addons/**/client/client.lua',
 }
 
 server_scripts {
   '@oxmysql/lib/MySQL.lua',
   'server/**/*',
-  'addons/**/server/server.lua',
 }
 
 files {
@@ -42,16 +43,3 @@ ui_page 'web/build/index.html'
 dependencies { 'mVehicle' }
 
 
--- escrow_ignore {
---   'framework.lua',
---   'Config.lua',
---   'DefaultGarages.lua',
---   'locales/*.json',
---   'server/**/*',
---   'client/**/*',
---   'web/build/index.html',
---   'web/build/**/*',
---   'addons/**/Config.lua',
---   'addons/**/*.lua',
---   'addons/**/*.lua',
--- }
